fc13f94ec2e18a2e724c413509dfb4ce2b74753f
Administrator <Administrator@CJ-201502090044>
2015-07-21 11:25:59 +0800

    修改上传操作

--- riceball/src/net/yasite/riceball/BaseService.java
+++ riceball/src/net/yasite/riceball/BaseService.java
@@ -45,72 +45,29 @@ public class BaseService {
 		}
 	}
 	
-//	public static Object postData(Context context,String url,
-//			Class entity,List<NameValuePair> list){
-//		return postData(context, url, entity, list, null);
+	
+//	public static Object uploadData(Context context,String url,
+//			Class entity,List<NameValuePair> list,List<NameValuePair> fileList,
+//			DataProcessing processing){
+//		BaseAPI api = new RequestAPI(context)
+//		.setMethod(url)
+//		.setEntity(entity)
+//		.setValuePair(list)
+//		.setFileList(fileList);
+//		try {
+//			if(api.doUpload()){
+//				if(processing != null){
+//					return processing.processing(api.getHandleResult());
+//				}
+//				return api.getHandleResult();
+//			}
+//		} catch (Exception e) {
+//		// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			return null;
+//		}
+//		return null;
 //	}
-	
-	public static Object postData(Context context,String url,
-			Class entity,List<NameValuePair> list,DataProcessing processing){
-		BaseAPI api = new RequestAPI(context)
-		.setMethod(url)
-		.setEntity(entity)
-		.setValuePair(list);
-		try {
-			if(api.doPost()){
-				if(processing != null){
-					return processing.processing(api.getHandleResult());
-				}
-				return api.getHandleResult();
-			}
-		} catch (Exception e) {
-		// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-		return null;
-	}
-	
-	
-	public static Object uploadData(Context context,String url,
-			Class entity,List<NameValuePair> fileList){
-		return uploadData(context, url, entity, 
-				new ArrayList<NameValuePair>(), fileList, null);
-	}
-	
-	public static Object uploadData(Context context,String url,
-			Class entity,List<NameValuePair> fileList,DataProcessing processing){
-		return uploadData(context,url,entity,
-				new ArrayList<NameValuePair>(),fileList,processing);
-	}
-	
-	public static Object uploadData(Context context,String url,
-			Class entity,List<NameValuePair> list,List<NameValuePair> fileList){
-		return uploadData(context, url, entity, list, fileList, null);
-	}
-	
-	public static Object uploadData(Context context,String url,
-			Class entity,List<NameValuePair> list,List<NameValuePair> fileList,
-			DataProcessing processing){
-		BaseAPI api = new RequestAPI(context)
-		.setMethod(url)
-		.setEntity(entity)
-		.setValuePair(list)
-		.setFileList(fileList);
-		try {
-			if(api.doPost()){
-				if(processing != null){
-					return processing.processing(api.getHandleResult());
-				}
-				return api.getHandleResult();
-			}
-		} catch (Exception e) {
-		// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-		return null;
-	}
 	
 	public interface DataProcessing{
 		public Object processing(Object handleResult);
